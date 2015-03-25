3c877b81e5031c23038e3946bf255ed1782b4af2
David Sinclair <david@sikhote.com>
2015-03-25 16:18:30 -0700

    Progress on select view.

---
+++ App/library/js/app.js
@@ -0,0 +1,9 @@
+(function($) {
+	
+	$(function() {
+		$.getJSON("library/js/people.js", function(json) {
+			
+		});
+	});
+	
+})(jQuery);
---
+++ App/library/js/people.js
@@ -0,0 +1,40 @@
+{
+	"people": [
+		{
+			"picture": "person-1.jpg"
+			"name": "John S.",
+			"location": "Detroit, MI",
+			"age": 34,
+			"sex": "Male",
+			"dataAvailable": {
+				"geneticsAvailable": true,
+				"fitnessDeviceActive": true,
+				"gpsTracking": true
+			}
+		},
+		{
+			"picture": "person-2.jpg"
+			"name": "Rebecca M.",
+			"location": "Arlington, VT",
+			"age": 27,
+			"sex": "Female",
+			"dataAvailable": {
+				"geneticsAvailable": true,
+				"fitnessDeviceActive": true,
+				"gpsTracking": true
+			}
+		},
+		{
+			"picture": "person-3.jpg"
+			"name": "Ruben M.",
+			"location": "Sacramento, CA",
+			"age": 32,
+			"sex": "Male",
+			"dataAvailable": {
+				"geneticsAvailable": true,
+				"fitnessDeviceActive": true,
+				"gpsTracking": true
+			}
+		}
+	]
+}
