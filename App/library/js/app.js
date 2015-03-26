59d2423333a235f9e911c13f3e00308b061eb98c
David Sinclair <david@sikhote.com>
2015-03-26 16:31:59 -0700

    Switching gears.

--- App/library/js/app.js
+++ App/library/js/app.js
@@ -7,5 +7,17 @@
 			console.log("Could not load people files.");
 		});
 		
+		$("#frame .view.personas ul.people").on("click", "li", function() {
+			var $this = $(this);
+			
+			$this.addClass("selected");
+			$("#frame .view.personas").removeClass("select").addClass("conversation");
+		});
+		
+		$("#frame .view.personas .side .back").on("click", function() {
+			$("#frame .view.personas ul.people li").removeClass("selected");
+			$("#frame .view.personas").removeClass("conversation").addClass("select");
+		})
+		
 	});
 })(jQuery);
