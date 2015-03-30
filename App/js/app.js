31cba31f1f41dd7f8cc07049d289f5c485f514af
David Sinclair <david@sikhote.com>
2015-03-30 14:58:54 -0700

    People area animations complete.

--- App/js/app.js
+++ App/js/app.js
@@ -7,36 +7,96 @@
 			console.log("Could not load people files.");
 		});
 		
-		// Randomly set sources to available after 2s
-		var findRandomSources = function() {
-			$("#frame .view.main.selecting .people .person .sources ul li").each(function() {
-				var $this = $(this);
-				
-				if(Math.round(Math.random())) {
-					$(this).addClass("available");
-				}
-				
-				$this.removeClass("waiting").addClass("done");
-			});
-		};
+		$("#frame button.refresh").on("click", function() {
+			location.reload();
+		});
 		
-		setTimeout(findRandomSources, 1000);
+		// Randomly set sources to available after 2s
+		(function() {
+			var findRandomSources = function() {
+				$("#frame .view.main.selecting .people .person .sources ul li").each(function() {
+					var $this = $(this);
+					
+					if(Math.round(Math.random())) {
+						$(this).addClass("available");
+					}
+					
+					$this.removeClass("waiting").addClass("done");
+				});
+			};
+			
+			setTimeout(findRandomSources, 1000);
+		})();
 		
 		// Allow swiping between people
-		$("#frame .view.main").swipe({
-			swipeStatus: function(event, phase, direction, distance) {
-				if(phase == "move") {
-					// Move all people with the swipe
-					$("#frame .view.main .people > ul").offset(0, distance);
-				}
-				
-				if(phase == "end") {
-					str="Handler fired, you swiped " + direction;
-				}
-			},
-			triggerOnTouchEnd: false,
-			threshold: 200
-		});
+		(function() {
+			var $peopleList = $("#frame .view.main .people > ul");
+			var peopleListOriginalPositionLeft = $peopleList.position().left;
+			var peopleListEdges = {
+				"left": 84,
+				"right": ($peopleList.find("> li").length - 1) * -640 + 84
+			};
+			var startingX;
+
+			$("#frame .view.main").swipe({
+				swipeStatus: function(event, phase, direction, distance) {
+					// Swipe initials
+					if(phase == "start") {
+						startingX = event.pageX;
+					}
+					
+					// Swipe in motion, move objects accordingly
+					if(phase == "move") {
+						var newPositionLeft = event.pageX - startingX + peopleListOriginalPositionLeft;
+						$("#frame .view.main .people > ul").offset({left: newPositionLeft});
+					}
+					
+					// Swipe over threshhold
+					if(phase == "end") {
+						// Check if at bounds and update the original position
+						
+						console.log(peopleListOriginalPositionLeft);
+						console.log(peopleListEdges.left);
+						console.log(peopleListEdges.right);
+						console.log("---");
+						
+						var setNewPosition = function() {
+							if(direction == "left") {
+								peopleListOriginalPositionLeft -= 640;
+							} else if(direction == "right") {
+								newPositionLeft = peopleListOriginalPositionLeft += 640;
+							}
+						}
+						
+						if(
+							peopleListOriginalPositionLeft < peopleListEdges.left &&
+							peopleListOriginalPositionLeft > peopleListEdges.right
+						) {
+							setNewPosition();
+						} else if(
+							peopleListOriginalPositionLeft == peopleListEdges.left &&
+							direction == "left"
+						) {
+							setNewPosition();
+						} else if(
+							peopleListOriginalPositionLeft == peopleListEdges.right &&
+							direction == "right"
+						) {
+							setNewPosition();
+						}
+						
+						$peopleList.animate({left: peopleListOriginalPositionLeft}, "fast", "easeOutBounce");
+					}
+					
+					// Swipe under threshhold
+					if(phase == "cancel") {
+						$peopleList.animate({left: peopleListOriginalPositionLeft}, "fast", "easeOutBounce");
+					}
+				},
+				triggerOnTouchEnd: false,
+				threshold: 300
+			});
+		})();
 		
 	});
 })(jQuery);
