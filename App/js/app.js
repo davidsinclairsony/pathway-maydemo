2a2976296bd7fc4a3bd791c9e847665d2ea12e44
David Sinclair <david@sikhote.com>
2015-04-01 16:21:13 -0700

    View updates and beginning routing.

--- App/js/app.js
+++ App/js/app.js
@@ -1,36 +1,110 @@
-(function($) {
- 	$(function() {
-		console.log("-");
-		
-		// Person model
-		var Person = Backbone.Model.extend({});
-		
-		// People collection
-		var People = Backbone.Collection.extend({
-			model: Person,
-			localStorage: new Backbone.LocalStorage("maydemo-backbone")
-		});
-		
-		var people = new People();
-		alert(1);
-		var cld_view=Backbone.View.extend({
-			el: 'template_div',
-			initialize: function() {
-				
-			},
-			render: function() {
-				// Compile the external template file using underscore
-				$.get(App.baseUrl + 'templates/your-template-file.html', function (data) {
-					template = _.template(data, {  });
-					this.$el.html(template);  
-				}, 'html');
-			}
-		});
-		
-		
-		
-		
-		// Load questions to model
-		
+$(function() {
+	console.log("-");
+	
+	// ------------------------------------
+	//	Models
+	
+	// Person model
+	var Person = Backbone.Model.extend({});
+	
+	// People collection
+	var People = Backbone.Collection.extend({
+		model: Person,
+		localStorage: new Backbone.LocalStorage("maydemo-backbone")
 	});
- })(jQuery);
+	
+	var people = new People();
+
+	// ------------------------------------
+	//	Views
+	
+	var AppView = Backbone.View.extend({
+		el: $("#app"),
+		events: {
+			"click .refresh": "refresh"
+		},
+		refresh: function() {
+			window.location.replace("/");
+		}
+	});
+	
+	var IntroView = Backbone.View.extend({
+		className: "view intro",
+		initialize: function() {
+			var self = this;
+			
+			self.render();
+			
+			// When the last element finishes animating
+			self.$el.one(
+				"webkitAnimationEnd oanimationend msAnimationEnd animationend",
+				"p",
+				function() {self.trigger("end");}
+			);
+		},
+		render: function() {
+			var self = this;
+
+			$.get("templates/intro.html", function(data) {
+				self.$el.html(data);
+			}, 'html');
+			
+			return self;
+		}
+	});
+	
+	var HelloView = Backbone.View.extend({
+		className: "view hello",
+		initialize: function() {
+			var self = this;
+			
+			self.render();
+		},
+		render: function() {
+			var self = this;
+
+			$.get("templates/hello.html", function(data) {
+				self.$el.html(data);
+			}, 'html');
+			
+			return self;
+		},
+		events: {
+			"click button": "end"
+		},
+		end: function() {
+			this.trigger("end");
+		}
+	});
+	
+	// ------------------------------------
+	//	Router
+	
+	var Router = Backbone.Router.extend({
+		routes: {
+			"": "intro",
+			"hello": "hello",
+			"conversation": "conversation",
+			'*error': 'error'
+		},
+		intro: function() {
+			console.log("home");
+		},
+		hello: function() {
+			console.log("hello");
+		},
+		conversation: function() {
+			console.log("conversation");
+		},
+		error: function() {
+			console.log("error");
+		}
+	});
+	
+	var app = new AppView();
+	var router = new Router();
+	Backbone.history.start({pushState: true});
+	
+	
+	
+});
--- App/js/main.js
+++ App/js/main.js
@@ -1,5 +1,3 @@
-// Require.js allows us to configure shortcut alias
-// There usage will become more apparent further along in the tutorial.
 require.config({
 	paths: {
 		jquery: "js/libraries/jquery-1.11.0.min.js",
@@ -8,14 +6,11 @@ require.config({
 		localStorage: "js/libraries/backbone.localStorage-min.js",
 		hammer: "js/libraries/hammer.min.js"
 	}
-
 });
 
 require([
-
-  // Load our app module and pass it to our definition function
-  'app',
-], function(App){
-  // The "app" dependency is passed in as "App"
-  App.initialize();
+	'app',
+], function(App) {
+	// The "app" dependency is passed in as "App"
+	App.initialize();
 });
