63096fc81c4218b4faedd754d092f3e359c98be8
David Sinclair <david@sikhote.com>
2015-04-27 14:20:00 -0700

    Completed sidebar of genes.

---
+++ js/json/genes.js
@@ -0,0 +1,10 @@
+[
+	{
+		"label": "Nutrition",
+		"list": ["ADIPOQ", "APOA2", "FTO", "KCTD10", "LIPC", "MMAB", "PPARG", "FADS1", "LEPR", "NMB", "TAS2R38", "ANKK1/DRD2", "SLC2A2", "CYP1A2", "TAS1R3", "MCM6", "ALDH2", "MTHFR", "NBPF3", "FUT2", "BCMO1", "SLC23A1", "GC", "intergenic", "MC4R", "ABCG8", "APOB", "CELSR2", "HMGCR", "LDLR", "MAFB", "NCAN", "PCSK9", "ABCA1", "ANGPTL4", "CETP", "GALNT2", "HNF4A", "LCAT", "LIPG", "LPL", "PLTP", "TTC39B", "ZNF259", "ANGPTL3", "GCKR", "LPL", "MLXIPL", "TRIB1", "XKR6", "ADCY5", "ADRA2A", "CRY2", "G6PC2", "GCK", "GLIS3", "MADD", "MTNR1B", "PROX1", "TCF7L2"]
+	},
+	{
+		"label": "Exercise",
+		"list": ["LIPC", "LPL", "PPARD", "INSIG2", "PPARGC1A", "MMP3", "FTO", "EDN1", "PPARD"]
+	}
+]
--- js/json/questions.js
+++ js/json/questions.js
@@ -1,15 +1,18 @@
 [
 	{
 		"id": 1,
-		"text": "How much exercise should I get today?"
+		"text": "How much exercise should I get today?",
+		"genes": 0
 	},
 	{
 		"id": 2,
-		"text": "What restaurants nearby serve food within my diet plan?"
+		"text": "What restaurants nearby serve food within my diet plan?",
+		"genes": 1
 	},
 	{
 		"id": 3,
-		"text": "What kinds of foods should I eat?"
+		"text": "What kinds of foods should I eat?",
+		"genes": 1
 	},
 	{
 		"id": 4,
