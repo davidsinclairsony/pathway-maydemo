e1e76ced0871606be59078def9407fc54fd2ab67
David Sinclair <david@sikhote.com>
2015-04-29 15:07:38 -0700

    Updated gene associations.

--- scripts/json/questions.js
+++ scripts/json/questions.js
@@ -2,17 +2,17 @@
 	{
 		"id": 1,
 		"text": "How much exercise should I get today?",
-		"genes": 0
+		"genes": 1
 	},
 	{
 		"id": 2,
 		"text": "What restaurants nearby serve food within my diet plan?",
-		"genes": 1
+		"genes": 0
 	},
 	{
 		"id": 3,
 		"text": "What kinds of foods should I eat?",
-		"genes": 1
+		"genes": 0
 	},
 	{
 		"id": 4,
